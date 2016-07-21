41fc56bc2e8f231035f172e5f26e6af4036a1213
Alun Morgan <alun.morgan@diamond.ac.uk>
2016-07-21 15:16:56 +0100

    Changing the name so as not to call it test

--- test_project/test_package/setup.py
+++ test_project/test_package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-      name="test_package",
+      name="first_code",
       author = "Alun Morgan",
       version = "0.1",
       packages = find_packages(),
